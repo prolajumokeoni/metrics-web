589aa71d412ae3fbd9e38a317d921a0cb4ac03ab
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-12 20:46:25 +0100

    Update logger in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import posts from './posts/posts';
 
-const middleware = [thunk];
+const middleware = [thunk, logger];
 
 const reducer = combineReducers({
   posts,
