9dbc3beba090dcdc9d46cd4bbf05b05b3bdd9f33
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-12 18:04:55 +0100

    Add navigation page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './components/Nav';
+import Posts from './components/Posts';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        hi
+        <Nav />
+        <Posts />
       </header>
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Nav = () => (
+  <div>
+    nav
+  </div>
+);
+
+export default Nav;
