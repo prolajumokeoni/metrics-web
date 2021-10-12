b7f930b034a820a88c3c1dfff1434f9433de35ef
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-12 18:03:27 +0100

    Add provider and store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
