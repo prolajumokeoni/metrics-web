484ea9674cb034de8655241be3c5882e3c1b9cfd
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-13 16:18:30 +0100

    Add tests

---
+++ src/__tests__/Posts.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../redux/store';
+import Posts from '../components/Posts';
+import SinglePost from '../components/SinglePost'
+
+test('Check if Home is rendering correctly', () => {
+  const body = document.createElement('tbody');
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Posts />
+      </BrowserRouter>
+    </Provider>,
+    body,
+  );
+expect(screen).toMatchSnapshot();
+});
+test('Check if Details is rendering correctly', () => {
+  const body = document.createElement('tbody');
+  const post = {
+    id: 5,
+    userId: 5,
+    title: '1000',
+    body: 'aannn',
+  };
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <SinglePost post={post} />
+      </BrowserRouter>
+    </Provider>,
+    body,
+  );
+  expect(screen).toMatchSnapshot();
+});
