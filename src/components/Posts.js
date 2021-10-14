5ea31ea672ba7d1427ad06c0d63ec34484c7387f
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-14 16:53:23 +0100

    Add filter and sytle

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPosts } from '../redux/posts/posts';
 import PostForm from './PostForm';
@@ -19,6 +19,10 @@ const Posts = () => {
     }
     return 0;
   });
+
+  const [postSearch, setpostSearch] = useState('');
+  const dataSearch = (str) => setpostSearch(str);
+
   return (
     <div>
       <div className="row px-2 fs-4 m-0 p-0">
@@ -37,17 +41,19 @@ const Posts = () => {
         All Blog Posts
         {data.length}
       </div>
+      <input type="text" className="search-pink" onChange={(e) => dataSearch(e.target.value)} placeholder="Search for post title" />
       <div className="row m-0 p-0">
-        {data.map((post) => (
-          <div
-            className="col-6 col-lg-4 border-pink border-round p-3 text-center blog"
-            key={post.id}
-          >
-            <i className="fas fa-blog fs-1 fw-bold pl-3" />
-            {' '}
-            <PostForm post={post} />
-          </div>
-        ))}
+        {data.filter((value) => value.title.toLowerCase().includes(postSearch.toLowerCase()))
+          .map((post) => (
+            <div
+              className="col-6 col-lg-4 border-pink border-round p-3 text-center blog"
+              key={post.id}
+            >
+              <i className="fas fa-blog fs-1 fw-bold pl-3" />
+              {' '}
+              <PostForm post={post} />
+            </div>
+          ))}
       </div>
     </div>
   );
