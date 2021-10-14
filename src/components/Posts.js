ca2257a046001081441410b41cab739a4deb6938
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-14 10:48:29 +0100

    Add sort and hover

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -10,11 +10,21 @@ const Posts = () => {
   useEffect(() => {
     dispatch(getPosts());
   }, [dispatch]);
-  const data = posts.sort((a, b) => b.title - a.title);
+  const data = posts.sort((a, b) => b.title.length - a.title.length);
   return (
     <div>
+      <div className="row px-2 fs-4 m-0 p-0">
+        <div className="col">2021</div>
+        <div className="col text-center">Blog</div>
+        <div className="col">
+          <div className="float-end">
+            <i className="fas fa-microphone me-3" />
+            <i className="fas fa-cog" />
+          </div>
+        </div>
+      </div>
       <div className="border-pink p-5 text-center fs-1 fw-bold">
-        <i className="fas fa-blog text-center" />
+        <i className="fab fa-blogger-b" />
         {' '}
         {' '}
         All Blog Posts
@@ -23,8 +33,9 @@ const Posts = () => {
       </div>
       <div className="row m-0 p-0">
         {data.map((post) => (
-          <div className="col-6 col-lg-4 border-pink border-round p-3" key={post.id}>
-            <i className="fab fa-hubspot fs-1 fw-bold text-center pl-3" />
+          <div className="col-6 col-lg-4 border-pink border-round p-3 text-center blog" key={post.id}>
+            <i className="fas fa-blog fs-1 fw-bold pl-3" />
+            {' '}
             <PostForm post={post} />
           </div>
         ))}
