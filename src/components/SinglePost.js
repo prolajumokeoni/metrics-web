b8c8c6602e027078fdb46f2ae6b29b6e75f5c3bc
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-14 14:49:49 +0100

    Delete text

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -3,21 +3,12 @@ import { NavLink } from 'react-router-dom';
 
 const SinglePost = ({ post }) => (
   <div className="p-2">
-    <div className="d-flex justify-content-between px-5 fw-bold mt-1">
-      <p>Blog Post</p>
-      <NavLink to="/" className="text-decoration-none text-white">
-        <i className="fas fa-arrow-left" />
-        {' '}
-        Back
-      </NavLink>
-    </div>
+    <NavLink to="/" className="text-decoration-none text-white">
+      <i className="fas fa-arrow-left" />
+    </NavLink>
     <hr />
-    <div className="p-2 text-center fw-bold fs-2">
-      {post.title}
-    </div>
-    <div className="article">
-      {post.body}
-    </div>
+    <div className="p-2 text-center fw-bold fs-2">{post.title}</div>
+    <div className="article">{post.body}</div>
   </div>
 );
 
